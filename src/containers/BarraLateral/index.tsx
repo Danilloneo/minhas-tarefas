3981dcc21ec07b74f005c24cb80ba424b617e04a
Danillo Guimarães <151641788+Danilloneo@users.noreply.github.com>
2024-04-11 22:22:07 -0300

    reta final projeto

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,20 +1,54 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Buscar" />
-    </div>
-    <S.Filtros>
-      <FiltroCard />
-      <FiltroCard />
-      <FiltroCard />
-      <FiltroCard />
-      <FiltroCard />
-      <FiltroCard ativo />
-    </S.Filtros>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Buscar"
+          value={termo}
+          onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
+        />
+      </div>
+      <S.Filtros>
+        <FiltroCard
+          valor={enums.Status.PENDENTE}
+          criterio="status"
+          legenda="pendentes"
+        />
+        <FiltroCard
+          valor={enums.Status.CONCLUIDA}
+          criterio="status"
+          legenda="conluídas"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.URGENTE}
+          criterio="prioridade"
+          legenda="urgentes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.IMPORTANTE}
+          criterio="prioridade"
+          legenda="importantes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.NORMAL}
+          criterio="prioridade"
+          legenda="normal"
+        />
+        <FiltroCard criterio="todas" legenda="todas" />
+      </S.Filtros>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
---
+++ src/containers/ListaDeTarefas/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  padding: 0 40px;
+  height: 100vh;
+  overflow-y: scroll;
+`
+export const Resultado = styled.p`
+  display: blocl;
+  font-weight: bold;
+  font-size: 18px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`
